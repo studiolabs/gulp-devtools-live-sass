2149965024c5a3d747f5d838db17b5451274a177
Monteiro Steed <steed.monteiro@gmail.com>
2017-03-30 14:30:06 +0200

    update

--- sass.js
+++ sass.js
@@ -1,260 +1,255 @@
-'use strict';
+"use strict";
 
-var path = require('path');
-var fs = require('fs');
-var utf8 = require('utf8');
-var Module = require('module');
-var SassUnpackModule = require('sass-unpack');
-var through2 = require('through2');
-var ES = require('event-stream');
-var File = require('vinyl');
+var path = require("path");
+var fs = require("fs");
+var utf8 = require("utf8");
+var Module = require("module");
+var SassUnpackModule = require("devtools-live-sass-unpack");
+var through2 = require("through2");
+var ES = require("event-stream");
+var File = require("vinyl");
 
-var sourceMap = require('source-map');
-var convertSourceMap = require('convert-source-map');
+var sourceMap = require("source-map");
+var convertSourceMap = require("convert-source-map");
 
 var sassUnpack;
 
 function SassDevTools(options) {
-	this.src = path.resolve(options.src);
-	this.cmd = options.cmd;
-	this.stream = null;
+  this.src = path.resolve(options.src);
+  this.cmd = options.cmd;
+  this.stream = null;
 }
 
 SassDevTools.prototype.init = function(devtoolsLive) {
+  this.output = devtoolsLive.options.devtools.destination;
+  sassUnpack = new SassUnpackModule({
+    file: this.src,
+    output: devtoolsLive.options.devtools.destination,
+    directory: devtoolsLive.options.devtools.directory,
+    map: true,
+    sourcemap: true,
+    mkdir: process.fs.mkdirpSync.bind(process.fs),
+    write: process.fs.writeFileSync.bind(process.fs)
+  });
 
-	this.output = devtoolsLive.options.devtools.destination;
-	sassUnpack = new SassUnpackModule({
-		file: this.src,
-		output: devtoolsLive.options.devtools.destination,
-		directory: devtoolsLive.options.devtools.directory,
-		map: true,
-		sourcemap:true,
-		mkdir: process.fs.mkdirpSync.bind(process.fs),
-		write: process.fs.writeFileSync.bind(process.fs)
-	});
+  this.devtoolsLive = devtoolsLive;
 
-
-	this.devtoolsLive = devtoolsLive;
-
-	var sassMap = sassUnpack.unpack();
-	return this.loadMap(sassMap);
+  var sassMap = sassUnpack.unpack();
+  return this.loadMap(sassMap);
 };
 
 SassDevTools.prototype.loadMap = function(sassMap) {
-	process.live['sass'] = "";
-	var map = sassMap.map;
+  process.live["sass"] = "";
+  var map = sassMap.map;
 
-	this.devtoolsLive.sassLinks = sassMap.links;
-	var today = new Date().getTime();
-	this.tasks = 0;
+  this.devtoolsLive.sassLinks = sassMap.links;
+  var today = new Date().getTime();
+  this.tasks = 0;
 
+  for (var i in map) {
+    map[i].plugin = this;
+    map[i].output = this.output + "/" + map[i].url;
 
-	for (var i in map) {
-		map[i].plugin = this;
-		map[i].output = this.output + '/' + map[i].url;
+    this.devtoolsLive.registerFile(map[i]);
 
-		this.devtoolsLive.registerFile(map[i]);
+    var sassDevToolsTmpFile = new SassDevToolsFile(
+      this.devtoolsLive,
+      map[i],
+      this
+    );
+    this.cmd(
+      sassDevToolsTmpFile.createFileStream(),
+      sassDevToolsTmpFile.createWriteStream(),
+      this.devtoolsLive.onError
+    );
 
-		var  sassDevToolsTmpFile = new SassDevToolsFile(this.devtoolsLive, map[i], this);
-		this.cmd(
-			sassDevToolsTmpFile.createFileStream(),
-			sassDevToolsTmpFile.createWriteStream(),
-			this.devtoolsLive.onError
-		);
+    process.live["sass"] += "\n<link rel='stylesheet' href='/" +
+      map[i].url +
+      "?" +
+      today +
+      "' />";
+  }
 
-		process.live['sass'] += "\n<link rel='stylesheet' href='/" + map[i].url + "?" + today + "' />";
-	}
+  for (var i in sassMap.links) {
+    var parent = this.devtoolsLive.tmp[sassMap.links[i]];
 
+    var file = {
+      path: i,
+      url: parent.url,
+      tmp: parent.tmp,
+      src: i.replace(sassUnpack.sourceDir, "")
+    };
+    file.plugin = this;
+    file.output = this.output + "/" + file.url;
 
-	for (var i in sassMap.links) {
+    this.devtoolsLive.registerFile(file);
+  }
 
-		var parent = this.devtoolsLive.tmp[sassMap.links[i]];
-
-		var file = {
-		    path: i,
-	    	url: parent.url,
-	      	tmp : parent.tmp,
-	      	src : i.replace(sassUnpack.sourceDir, '')
-		};
-		file.plugin = this;
-		file.output = this.output + '/' + file.url;
-
-		this.devtoolsLive.registerFile(file);
-
-	}
-
-	if(map.length == 0){
-		this.devtoolsLive.streamFinished(this);
-	}
-
+  if (map.length == 0) {
+    this.devtoolsLive.streamFinished(this);
+  }
 };
 
 SassDevTools.prototype.resolve = function(devtoolsLive, file) {
+  if (file.path == this.src) {
+    sassUnpack.unpack();
+  }
 
-	if(file.path == this.src){
-		sassUnpack.unpack();
-	}
-
-	this.exec(devtoolsLive, file);
-
+  this.exec(devtoolsLive, file);
 };
 
 SassDevTools.prototype.exec = function(devtoolsLive, filepath) {
+  var sassDevToolsTmpFile = new SassDevToolsFile(devtoolsLive, filepath, this);
 
-
-	var  sassDevToolsTmpFile = new SassDevToolsFile(devtoolsLive, filepath, this);
-
-	this.cmd(
-		sassDevToolsTmpFile.createFileStream(),
-		sassDevToolsTmpFile.createWriteAndPushStream(),
-		devtoolsLive.onError
-	);
-
+  this.cmd(
+    sassDevToolsTmpFile.createFileStream(),
+    sassDevToolsTmpFile.createWriteAndPushStream(),
+    devtoolsLive.onError
+  );
 };
 
 function SassDevToolsFile(devtoolsLive, file, sassDevTools) {
-	this.file = file;
-	this.devtoolsLive = devtoolsLive;
-	this.sassDevTools = sassDevTools;
+  this.file = file;
+  this.devtoolsLive = devtoolsLive;
+  this.sassDevTools = sassDevTools;
 }
 
-SassDevToolsFile.prototype.cleanSourceMap = function(sassContent, sourceContent) {
+SassDevToolsFile.prototype.cleanSourceMap = function(
+  sassContent,
+  sourceContent
+) {
+  var fileSourceMap = null;
+  var generator = new sourceMap.SourceMapGenerator({
+    file: "/" + this.file.url
+  });
 
-	var fileSourceMap = null;
-	var generator = new sourceMap.SourceMapGenerator({
-		file: '/' + this.file.url
-	});
+  var sourcemap = convertSourceMap.fromSource(sassContent, true);
 
-	var sourcemap = convertSourceMap.fromSource(sassContent, true);
+  sourcemap = sourcemap.toObject();
 
-	sourcemap = sourcemap.toObject();
+  var consumer = new sourceMap.SourceMapConsumer(sourcemap);
 
-	var consumer = new sourceMap.SourceMapConsumer(sourcemap);
+  consumer.eachMapping(
+    function(m) {
+      var path = m.source.replace(sourcemap.sourceRoot, "");
+      var filepath = Module._findPath(path, [
+        sassUnpack.rootDir,
+        sassUnpack.sourceDir
+      ]).replace(sassUnpack.sourceDir, "");
+      generator.addMapping({
+        source: "/" + filepath,
+        original: { line: m.originalLine, column: m.originalColumn },
+        generated: { line: m.generatedLine, column: m.generatedColumn }
+      });
+    }.bind(this),
+    {},
+    consumer.ORIGINAL_ORDER
+  );
 
-	consumer.eachMapping(function(m) {
+  for (var i in sourcemap.sources) {
+    var path = sourcemap.sources[i]
+      .replace(sourcemap.sourceRoot, "")
+      .replace(sassUnpack.rootDir, "");
+    path = Module._findPath(path, [sassUnpack.rootDir, sassUnpack.sourceDir]);
+    var filepath = path.replace(sassUnpack.sourceDir, "");
 
- 		var path = m.source.replace(sourcemap.sourceRoot, '');
-		var filepath =  Module._findPath(path, [sassUnpack.rootDir, sassUnpack.sourceDir]).replace(sassUnpack.sourceDir, '');
-		generator.addMapping({
-						  source: '/'+filepath,
-						  original: { line: m.originalLine, column: m.originalColumn },
-						  generated: { line: m.generatedLine , column: m.generatedColumn }
-						});
+    sourcemap.sources[i] = filepath;
+  }
 
-	}.bind(this), {}, consumer.ORIGINAL_ORDER);
+  sourcemap.mappings = generator.toJSON().mappings;
+  sourcemap.file = "/" + this.file.url;
 
-	for(var i in sourcemap.sources){
+  sourcemap.sourceRoot = "/";
 
- 		var path = sourcemap.sources[i].replace(sourcemap.sourceRoot, '').replace(sassUnpack.rootDir, '');
- 		path = Module._findPath(path, [sassUnpack.rootDir, sassUnpack.sourceDir]);
- 		var filepath =  path.replace(sassUnpack.sourceDir, '');
-
- 		sourcemap.sources[i]=filepath;
-	}
-
-	sourcemap.mappings = generator.toJSON().mappings;
-	sourcemap.file = '/' + this.file.url;
-
-	sourcemap.sourceRoot = '/';
-
-	return convertSourceMap.fromObject(sourcemap).toComment({multiline:true});
-
+  return convertSourceMap.fromObject(sourcemap).toComment({ multiline: true });
 };
 
 SassDevToolsFile.prototype.saveFile = function(filepath, sassContent) {
-	var inline = this.cleanSourceMap(sassContent);
-	var content = convertSourceMap.removeComments(sassContent);
-	process.fs.writeFileSync(filepath, content+'\n'+ inline);
+  var inline = this.cleanSourceMap(sassContent);
+  var content = convertSourceMap.removeComments(sassContent);
+  process.fs.writeFileSync(filepath, content + "\n" + inline);
 
-	if(this.sassDevTools.tasks > 0){
-		this.sassDevTools.tasks--;
-		if(this.sassDevTools.tasks == 0){
-			this.devtoolsLive.streamFinished(this.sassDevTools);
-		}
-	}
+  if (this.sassDevTools.tasks > 0) {
+    this.sassDevTools.tasks--;
+    if (this.sassDevTools.tasks == 0) {
+      this.devtoolsLive.streamFinished(this.sassDevTools);
+    }
+  }
 
-	return content;
-}
+  return content;
+};
 
 SassDevToolsFile.prototype.pushFile = function(sassContent) {
+  var record = {
+    action: "update",
+    url: this.devtoolsLive.getClientPageUrl() + this.file.url
+  };
 
-	var record = {
-			action: 'update',
-			url: this.devtoolsLive.getClientPageUrl() + this.file.url
-		};
+  if (this.file.content === undefined) {
+    record.sync = this.devtoolsLive.getClientHostname() + "/" + this.file.src;
+  } else {
+    record.resourceName = this.devtoolsLive.getClientHostname() +
+      "/" +
+      this.file.src;
+    delete this.file.content;
+  }
 
-		if (this.file.content === undefined) {
-			record.sync = this.devtoolsLive.getClientHostname() + '/' + this.file.src;
-		} else {
-			record.resourceName = this.devtoolsLive.getClientHostname() + '/' + this.file.src;
-			delete this.file.content;
-		}
+  record.event = this.file.src;
 
-		record.event = this.file.src;
+  record.content = this.saveFile(this.file.output, sassContent);
 
-		record.content = this.saveFile(this.file.output, sassContent);
+  this.devtoolsLive.broadcast(record);
 
-		this.devtoolsLive.broadcast(record);
-
-
-		if(this.sassDevTools.tasks > 0){
-			this.sassDevTools.tasks--;
-			if(this.sassDevTools.tasks == 0){
-				this.devtoolsLive.streamFinished(this.sassDevTools);
-			}
-		}
-
-
-
+  if (this.sassDevTools.tasks > 0) {
+    this.sassDevTools.tasks--;
+    if (this.sassDevTools.tasks == 0) {
+      this.devtoolsLive.streamFinished(this.sassDevTools);
+    }
+  }
 };
 
 SassDevToolsFile.prototype.createWriteAndPushStream = function() {
-	 var modifyFile = function(file) {
-	 	if (file.contents.length > 0) {
-		this.pushFile(file.contents.toString());
-	 	}else {
-		this.pushFile('/** empty **/');
-	 	}
+  var modifyFile = function(file) {
+    if (file.contents.length > 0) {
+      this.pushFile(file.contents.toString());
+    } else {
+      this.pushFile("/** empty **/");
+    }
   }.bind(this);
 
-	return ES.through(modifyFile);
-
+  return ES.through(modifyFile);
 };
 
 SassDevToolsFile.prototype.createWriteStream = function() {
-
-	 var modifyFile = function(file) {
-	 	if (file.contents.length > 0) {
-		this.saveFile(this.file.output,  file.contents.toString());
-	 	}else {
-		this.saveFile(this.file.output, '/** empty **/');
-	 	}
+  var modifyFile = function(file) {
+    if (file.contents.length > 0) {
+      this.saveFile(this.file.output, file.contents.toString());
+    } else {
+      this.saveFile(this.file.output, "/** empty **/");
+    }
   }.bind(this);
 
-	return ES.through(modifyFile);
+  return ES.through(modifyFile);
 };
 
 SassDevToolsFile.prototype.createFileStream = function() {
+  var data = process.fs.readFileSync(this.file.tmp);
 
-	var data = process.fs.readFileSync(this.file.tmp);
+  this.sassDevTools.tasks++;
 
-	this.sassDevTools.tasks++;
+  var file = new File({
+    path: this.file.src,
+    contents: data instanceof Buffer ? data : new Buffer(data)
+  });
 
+  var stream = through2.obj(function(file, enc, callback) {
+    this.push(file);
+    return callback();
+  });
 
-	var file = new File({
-		path: this.file.src,
-		contents: ((data instanceof Buffer) ? data : new Buffer(data))
-	});
+  stream.write(file);
 
-	var stream = through2.obj(function(file, enc, callback) {
-		this.push(file);
-		return callback();
-	});
-
-	stream.write(file);
-
-	return stream;
-
+  return stream;
 };
 
 module.exports = SassDevTools;
